95a2a9766445a34c2408edb02959be28c3c7fa4b
murmota <nega.kuz@gmail.com>
2025-07-30 20:07:36 +0300

    ww

--- init-postgres.sh
+++ init-postgres.sh
@@ -7,6 +7,11 @@ chown -R postgres:postgres ./logs/pg
 mkdir -p ./logs/app/$(date +%F)
 chown -R postgres:postgres ./logs/app
 
-echo "host all all 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf
+cat > /var/lib/postgresql/data/pg_hba.conf <<EOF
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+local   all             all                                     trust
+host    all             all             0.0.0.0/0               trust
+host    all             all             ::/0                    trust
+EOF
 
 exec docker-entrypoint.sh postgres
