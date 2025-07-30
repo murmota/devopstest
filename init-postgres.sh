e58655a9796a31d4d43362929d5297d2dddeea2b
murmota <nega.kuz@gmail.com>
2025-07-30 23:26:47 +0300

    durka s logs continue

--- init-postgres.sh
+++ init-postgres.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-mkdir -p /var/lib/postgresql/data/logs
-
-cat > /var/lib/postgresql/data/pg_hba.conf <<EOF
-# TYPE  DATABASE        USER            ADDRESS                 METHOD
+cat > "$PGDATA/pg_hba.conf" <<EOF
 local   all             all                                     trust
 host    all             all             0.0.0.0/0               trust
 host    all             all             ::/0                    trust
 EOF
 
-cat > /var/lib/postgresql/data/postgresql.conf <<EOF
+cat >> "$PGDATA/postgresql.conf" <<EOF
 listen_addresses = '*'
 logging_collector = on
-log_directory = 'logs'
+log_directory = '/var/log/postgresql'
 log_filename = 'postgresql-%Y-%m-%d.log'
 log_truncate_on_rotation = on
 log_rotation_age = 1d
